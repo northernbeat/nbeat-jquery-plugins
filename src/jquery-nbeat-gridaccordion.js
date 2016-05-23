a605f4cb04bb821acc8645f5e8ae9ea7d4757ed0
Eirik Refsdal <eirikref@gmail.com>
2016-05-23 15:26:46 +0200

    Handle resize and add checking of number of items per line

--- src/jquery-nbeat-gridaccordion.js
+++ src/jquery-nbeat-gridaccordion.js
@@ -13,7 +13,6 @@
 
     $.nbeatgridaccordion = function(grid, options) {
         this.init(grid, options);
-        console.log("grid accordion");
     };
 
 
@@ -54,10 +53,40 @@
         grid: null,
 
         /**
-         * @property {jquery} list
+         * @property {jquery} items
          *           Internal list of items
          */
-        listitems: [],
+        items: {},
+
+        /**
+         * @property {jquery} subId
+         *           #id of the currently used menu element
+         */
+        subId: 0,
+
+        /**
+         * @property {jquery} subPosition
+         *           Current position of the submenu
+         */
+        subPosition: 0,
+
+        /**
+         * @property {jquery} numitems
+         *           Size of this.items
+         */
+        numitems: 0,
+
+        /**
+         * @property {jquery} perline
+         *           Calculated number of items per line
+         */
+        perline: 1,
+        
+        /**
+         * @property {jquery} resizeTimer
+         *           Timer for the window.resize handling
+         */
+        resizeTimer: null,
 
 
 
@@ -74,6 +103,7 @@
             this.settings = $.extend({}, this.settings, options);
             this.grid = $(grid);
             this.buildItemList();
+            this.calculateItemsPerLine();
             this.handleEvents();
         },
 
@@ -84,15 +114,37 @@
             var self = this;
             var i    = 0;
             
-            self.grid.children().each(function() {
+            this.grid.children().each(function() {
                 var id = $(this)[0]["id"];
-                // console.log($(this));
-                // console.log(self);
-                self.listitems[id] = i;
+                self.items[id] = i;
                 ++i;
             });
 
-            // console.log(this.listitems);
+            this.numitems = i;
+        },
+
+
+
+        calculateItemsPerLine: function()
+        {
+            var el         = $(".list-item", this.grid).first();
+            var elWidth    = el.width();
+            var gridWidth  = this.grid.width();
+            var percentage = Math.round((elWidth / gridWidth) * 100);
+            
+            if (percentage <= 21) {
+                this.perline = 5;
+            } else if (percentage <= 26) {
+                this.perline = 4;
+            } else if (percentage <= 34) {
+                this.perline = 3;
+            } else if (percentage <= 51) {
+                this.perline = 2;
+            } else {
+                this.perline = 1;
+            }
+
+            this.log("Calc items per line: " + this.perline);
         },
 
         
@@ -108,60 +160,125 @@
         {
             var self = this;
 
-            $(".title", self.grid).on("click", function(e) {
-                var parent    = e.currentTarget.parentNode;
-                var parentId  = parent.id;
-                var parentPos = self.listitems[parentId];
-                var expPos    = (Math.floor(parentPos / 4) * 4) + 3;
+            $(window).resize(function() {
+                clearTimeout(self.resizeTimer);
+                self.resizeTimer = setTimeout(function() {
+                    self.calculateItemsPerLine();
+                    self.repositionSubMenu();
+                }, 250);
+            });
 
-                self.markSelected(parentId, parentPos);
-                self.insertExpanded(parentId, expPos);
+            $(".list-item", self.grid).on("click", function(e) {
+                if ($(this).hasClass("is-selected")) {
+                    self.resetGrid();
+                } else {
+                    self.subId = e.currentTarget.id;
+                    self.markSelected();
+                    self.insertSubMenu();
+                }
             });
         },
 
 
+        
+        calculateSubMenuPosition: function()
+        {
+            var pos        = this.items[this.subId];
+            var subPos     = 0;
+            var el         = $("#" + this.subId);
 
+            subPos = (Math.floor(pos / this.perline) * this.perline) + (this.perline - 1);
+
+            if (subPos >= this.numitems) {
+                subPos = this.numitems - 1;
+            }
+
+            return subPos;
+        },
+
+
+
+        repositionSubMenu: function()
+        {
+            if ($(".expanded-category", this.grid).length < 1) {
+                return;
+            }
+            
+            var newPos = this.calculateSubMenuPosition();
+            var el     = $(".expanded-category", this.grid);
+
+            this.log("Submenu position. Current: " + this.subPosition + ". New: " + newPos);
+            
+            if (newPos != this.subPosition) {
+                el.detach();
+                $(".list-item", this.grid).eq(newPos).after(el);
+                this.subPosition = newPos;
+            }
+
+            this.markSelected();
+        },
+
+        
+
+        /**
+         * 
+         */
+        resetGrid: function()
+        {
+            this.removeSubMenu();
+            $(".list-item", self.grid).removeClass("is-selected is-selected-neighbour");
+        },
+
+
+        
         /**
          * Mark selected menu item
          */
-        markSelected: function(id, pos)
+        markSelected: function()
         {
-            var startPos = Math.floor(pos / 4) * 4;
-            
-            $(".is-selected-neighbour", self.grid).removeClass("is-selected-neighbour");
+            var pos      = this.items[this.subId];
+            var startPos = Math.floor(pos / this.perline) * this.perline;
 
-            for (var i = startPos; i <= startPos + 3; ++i) {
+            console.log("her");
+            $(".is-selected-neighbour", self.grid).removeClass("is-selected-neighbour");
+            console.log("der");
+
+            for (var i = startPos; i <= startPos + (this.perline - 1); ++i) {
                 $(".list-item", this.grid).eq(i).addClass("is-selected-neighbour");
             }
 
             // var expPos = (Math.floor(parentPos/4) * 4) + 3;
             $(".list-item", self.grid).removeClass("is-selected");
-            $("#" + id).removeClass("is-selected-neighbour");
-            $("#" + id).addClass("is-selected");
+            $("#" + this.subId).removeClass("is-selected-neighbour");
+            $("#" + this.subId).addClass("is-selected");
         },
 
 
         /**
-         * Log debug messages
+         * Insert/show expanded list
          */
-        insertExpanded: function(id, pos)
+        insertSubMenu: function()
         {
+            var pos          = this.calculateSubMenuPosition(this.subId);
             var child        = $('<div>').addClass("expanded-category");
             var childContent = $('<ul>');
-            var origContent  = $("#" + id + " .list-item-children").html();
+            var origContent  = $("#" + this.subId + " .list-item-children").html();
 
-            console.log(origContent);
-            
-            // 1. Remove if exists
-            if ($(".expanded-category", this.grid).length) {
-                $(".expanded-category", this.grid).remove();
-            }
-            
-            // 2. TBD. Fetch proper child data
+            this.removeSubMenu();
             childContent.html(origContent);
             child.append(childContent);
-
             $(".list-item", this.grid).eq(pos).after(child);
+            child.slideDown();
+            this.subPosition = pos;
+        },
+
+
+        
+        removeSubMenu: function()
+        {
+            $(".expanded-category", self.grid).slideUp(function() {
+                $(this).remove();
+            });
         },
 
 
